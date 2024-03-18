4ab6713f4132fe4dd20e5a5fc2401e2925be5591
Muhammad Ibrahim <90500901+Ibrahim0320@users.noreply.github.com>
2024-03-18 16:11:39 +0000

    added description

--- BCG Gen AI project.py
+++ BCG Gen AI project.py
@@ -1,1 +1,5 @@
 # Boston Consulting Group Gen AI chatbot
+
+# A project focused on applying AI and data analysis skills to develop AI-powered tools, 
+# such as a financial chatbot, while learning to translate complex data into strategic business insights.
+
